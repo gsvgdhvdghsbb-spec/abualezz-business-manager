8ca03698d74f62fc84e23e2fc83b58a3416d6a1c
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 17:31:01 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material';
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(const AbuAlEzzApp());
@@ -15,21 +16,9 @@ class AbuAlEzzApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
-        fontFamily: 'Cairo',
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('نظام إدارة أبو العز (Abu Al-Ezz Manager)'),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text(
-            'مرحباً بك في نظام إدارة أبو العز V1.0\nجاهزون للربط والتطوير 🚀',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
+
